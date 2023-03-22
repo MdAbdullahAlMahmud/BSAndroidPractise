deaae9c23b3c2d4de1bfd80d0dfb69f02687e510
MdAbdullahAlMahmud <mmunna864@gmail.com>
2023-03-23 01:31:10 +0600

    Add guess High low project
       - Add function called checkWeatherUserGuessIsRightOrNot and its
           functionality

--- app/src/main/java/com/example/bsandroidpractise/MainActivity.java
+++ app/src/main/java/com/example/bsandroidpractise/MainActivity.java
@@ -3,39 +3,60 @@ package com.example.bsandroidpractise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText amountEdt;
-    private MaterialButton converteCurrencyButton;
+    private EditText numberEdt;
+    private MaterialButton guessButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        amountEdt = findViewById(R.id.amountEdt);
-        converteCurrencyButton = findViewById(R.id.converteCurrencyButton);
-        converteCurrencyButton.setOnClickListener(new View.OnClickListener() {
+        numberEdt = findViewById(R.id.numberEdt);
+        guessButton = findViewById(R.id.guessButton);
+
+
+        final int min = 1;
+        final int max = 20;
+        final int randomNumber = new Random().nextInt((max - min) + 1) + min;
+        Log.v("Number","Generated Random Number  --> "+ String.valueOf(randomNumber));
+
+
+        guessButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userEnterAmount = amountEdt.getText().toString();
-                if (userEnterAmount.isEmpty()){
-                    amountEdt.setError("required");
+                String userEnterNumber = numberEdt.getText().toString();
+                if (userEnterNumber.isEmpty()){
+                    numberEdt.setError("required");
                     return;
                 }
-                double amount = Double.parseDouble(userEnterAmount);
-                double amountInEuro = amount * 1.3;
-                String euroSymbol = "\u20ac";
-                Toast.makeText(MainActivity.this, userEnterAmount+" is = "+euroSymbol + " "+ amountInEuro , Toast.LENGTH_SHORT).show();
+                int userGuessNumber = Integer.parseInt(userEnterNumber);
+                checkWeatherUserGuessIsRightOrNot(userGuessNumber,randomNumber);
+
 
             }
         });
 
+    }
 
 
+    private void checkWeatherUserGuessIsRightOrNot(int userGuessNumber, int randomNumber){
+        if (userGuessNumber>randomNumber){
+            Toast.makeText(MainActivity.this,"Your guess is higher",Toast.LENGTH_SHORT).show();
+            return;
+        }else if (userGuessNumber<randomNumber){
+            Toast.makeText(MainActivity.this,"Your guess is lower",Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            Toast.makeText(MainActivity.this,"Congrats you got me",Toast.LENGTH_SHORT).show();
 
+        }
     }
 }
