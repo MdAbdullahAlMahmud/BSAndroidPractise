f3a0d49855f07a71a33eef3d3f6d7e820375e257
Abdullah Al Mahmud <mmunna864@gmail.com>
2023-03-23 17:50:49 +0600

    Add guess celebraties project   - video course content is too old that parsing url doesn't exists any     more

--- app/src/main/java/com/example/bsandroidpractise/MainActivity.java
+++ app/src/main/java/com/example/bsandroidpractise/MainActivity.java
@@ -2,13 +2,77 @@ package com.example.bsandroidpractise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> nameList = new ArrayList<>();
+    private ArrayList<String> imageList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        DownloadTask downloadTask  = new DownloadTask();
+        try {
+            //String webContent = downloadTask.execute("https://www.posh24.se/kandisar").get();
+            String webContent = downloadTask.execute("https://www.google.com").get();
+
+            Toast.makeText(this, "Download complete", Toast.LENGTH_SHORT).show();
+            Log.v("Content", " -> " + webContent);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+
+
+    public class DownloadTask extends AsyncTask<String,Void,String>{
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            StringBuilder result = new StringBuilder();
+            try {
+                URL url  = new URL(urls[0]);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                int data = reader.read();
+                while (data!=-1){
+                    char ch = (char) data;
+                    result.append(ch);
+                    data = reader.read();
+                }
+                return  reader.toString();
+
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
